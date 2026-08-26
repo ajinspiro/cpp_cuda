1ffa4d4ecb04c5bb8baa8bbe190470b30e13e219
mysticmallu <3ystic3allu@gmail.com>
2026-08-26 12:38:51 +0530

    changes

---
+++ lesson-02-interop/cpp_code/cpp_code.cpp
@@ -0,0 +1,143 @@
+// Provided by Praseed Pai K T
+// https://github.com/praseedpai/NativeInterfaceJavaCsharp/blob/master/cpp_code.cpp
+// Original cpp_code.cpp has been modified for x64 use here.
+
+#include <stdio.h>
+#include <cstring>
+#include <windows.h>
+
+using namespace std;
+
+////////////////////////////////////////
+//
+// Add
+//
+extern "C" __declspec(dllexport) int __cdecl Add(int a, int b)
+{
+    return a + b;
+}
+
+////////////////////////////////////////
+//
+// ANSI version of Length function
+//
+extern "C" __declspec(dllexport) size_t __cdecl StrLenA(char *r)
+{
+    return strlen(r);
+}
+
+////////////////////////////////////////
+//
+// Unicode version of Strlen
+//
+extern "C" __declspec(dllexport) size_t __cdecl StrLenW(wchar_t *r)
+{
+    return wcslen(r);
+}
+//////////////////////////////
+//
+// Computes the minimum value in an int array
+//
+extern "C" __declspec(dllexport) int __cdecl MinArray(int *pData, int length)
+{
+    int minData = pData[0];
+    for (int pos = 1; pos < length; pos++)
+    {
+        if (pData[pos] < minData)
+        {
+            minData = pData[pos];
+        }
+    }
+
+    return minData;
+}
+
+///////////////////////////////////////////
+//
+// Computes the minimum for a double array
+//
+extern "C" __declspec(dllexport) double __cdecl MinArrayD(double *pData, int length)
+{
+    double minData = pData[0];
+    for (int pos = 1; pos < length; pos++)
+    {
+        if (pData[pos] < minData)
+        {
+            minData = pData[pos];
+        }
+    }
+    return minData;
+}
+
+//////////////////////////////////////////
+//
+// Computes the arithematic mean of an array
+//
+extern "C" __declspec(dllexport) double __cdecl Average(double *pData, int length)
+{
+    double minData = pData[0];
+    for (int pos = 1; pos < length; pos++)
+    {
+        minData += pData[pos];
+    }
+    return minData / length;
+}
+
+/////////////////////////////////////////
+//
+// This structure will be passed between C# and C++
+//
+struct EventData
+{
+    int I;
+    char *Message;
+};
+
+extern "C" __declspec(dllexport) bool __cdecl PutEventData(EventData *ptr)
+{
+    printf("%s: %s\n", "PutEventData\0", ptr->Message);
+    printf("%s: %d\n", "PutEventData\0", ptr->I);
+    return false;
+}
+
+// This is not possible because when GetEventData exits, it will destroy the callstack
+// and msg and ed will go out of scope.
+// extern "C" __declspec(dllexport) EventData *__cdecl GetEventData()
+// {
+//     EventData ed = {};
+//     char msg[] = "Let's dare to segfault\0";
+//     ed.I = 321;
+//     ed.Message = msg;
+//     return &ed;
+// }
+
+extern "C" __declspec(dllexport) void __cdecl NumberTransformer(long(__cdecl *square_callback)(int rs))
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        double ret = (*square_callback)(i);
+        printf("NumberTransformer: f(%d)=%g\n", i, ret);
+    }
+}
+
+extern "C" __declspec(dllexport) bool __cdecl StringCopy(char *dest, int dest_size, const char *src)
+{
+    if (dest == nullptr || dest_size <= 0)
+        return false;
+
+    if (src == nullptr)
+    {
+        dest[0] = '\0';
+        return false;
+    }
+
+    size_t src_len = strlen(src);
+    if (src_len >= (size_t)dest_size)
+    {
+        dest[0] = '\0'; // fail cleanly rather than truncate silently
+        return false;
+    }
+
+    strcpy(dest, src);
+    return true;
+}
---
+++ lesson-02-interop/cpp_code/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+extern "C" __declspec(dllimport) int __cdecl Add(int a, int b);
+extern "C" __declspec(dllimport) size_t __cdecl StrLenA(char *r);
+extern "C" __declspec(dllimport) size_t __cdecl StrLenW(wchar_t *r);
+extern "C" __declspec(dllimport) int __cdecl MinArray(int *pData, int length);
+extern "C" __declspec(dllexport) bool __cdecl StringCopy(char *dest, int dest_size, const char *src);
+
+struct EventData
+{
+    int I;
+    char *Message;
+};
+extern "C" __declspec(dllimport) bool __cdecl PutEventData(EventData *ptr);
+extern "C" __declspec(dllimport) void __cdecl NumberTransformer(long(__cdecl *square_callback)(int rs));
+extern "C" __declspec(dllimport) bool __cdecl StringCopy(char *dest, int dest_size, const char *src);
+
+int main(int, char **)
+{
+    // Add
+    int a = 5, b = 6, c = Add(a, b);
+    std::cout << "Sum of " << a << ", " << b << " = " << c << std::endl;
+
+    // StrLenA
+    char name_ansi[] = "arun kumar\0";
+    size_t name_len_ansi = StrLenA(name_ansi);
+    std::cout << "ANSI string length of '" << name_ansi << "' = " << name_len_ansi << std::endl;
+
+    // StrLenW
+    wchar_t name_unicode[] = L"praseed pai 😎";
+    size_t name_len_unicode = StrLenW(name_unicode);
+    std::wcout << L"UNICODE string length = " << name_len_unicode << std::endl;
+
+    // MinArray
+    int int_arr[] = {0, 5, 2, 8, -1, 4, 5, 3}, int_arr_min = MinArray(int_arr, sizeof(int_arr) / sizeof(int));
+    std::cout << "Smallest element in array [";
+    for (size_t i = 0; i < sizeof(int_arr) / sizeof(int); i++)
+    {
+        std::cout << int_arr[i] << ",";
+    }
+    std::cout << "\b \b" << "] = " << int_arr_min << std::endl;
+
+    // PutEventData
+    char event_data_message[] = "MicroSoft Visual C++\0";
+    EventData ed = {};
+    ed.I = 123;
+    ed.Message = event_data_message;
+    PutEventData(&ed);
+
+    // NumberTransformer
+    auto create_squares = [](int x)
+    {
+        return long(x * x);
+    };
+    auto create_cubes = [](int x)
+    {
+        return long(x * x * x);
+    };
+    NumberTransformer(create_squares);
+    NumberTransformer(create_cubes);
+
+    // StringCopy
+    char source[] = "arun kumar\0", destination[sizeof(source)] = {};
+    bool result = StringCopy(destination, sizeof(destination), source);
+    std::cout << "StringCopy: Result=" << result << " Destination=" << destination << std::endl;
+    
+    return EXIT_SUCCESS;
+}
